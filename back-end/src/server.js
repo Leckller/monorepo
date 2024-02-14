d9ff3448f29cdbc91e65eb3e895c07e3acc90565
Leckller <7leckler@gmail.com>
2024-02-14 12:08:05 -0300

    feat: hello mysql with node server!

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const connection = require('./db/connection')
 
 const port = 6969;
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`server rodando na porta ${port}`)
+  const teste = await connection.execute('SELECT "hello mysql with node!"')
+  console.log(teste)
 });
