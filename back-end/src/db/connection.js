05680d56f038bdae3bc21d476c0edd090486b40b
Leckller <7leckler@gmail.com>
2024-02-20 16:01:32 -0300

    feat: novo banco de dados

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,14 +1,12 @@
-const mysql = require('mysql2/promise');
+const { getFirestore } = require('firebase-admin/firestore');
 
-const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 33060,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'tasksDB',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const admin = require("firebase-admin");
+const serviceAccount = require("../../serviceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
 });
 
-module.exports = connection;
+const db = getFirestore();
+
+module.exports = db;
