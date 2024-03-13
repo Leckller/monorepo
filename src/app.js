2f4510a7c66251f7ab7fafa3761bd93ae96fe243
Leckller <7leckler@gmail.com>
2024-03-13 18:08:55 -0300

    feat: morra

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => {
+
+app.get("/", (_req, res) => {
   res.status(200).json("Im Online!");
 });
 app.use("/users", routes.users);
