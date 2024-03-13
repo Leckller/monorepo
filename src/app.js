78a6f3f92f0268558f2e12ec0de6c1c553ed5e10
Leckller <7leckler@gmail.com>
2024-03-13 18:08:28 -0300

    feat: online

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-
+app.get("/", (req, res) => {
+  res.status(200).json("Im Online!");
+});
 app.use("/users", routes.users);
 app.use("/login", routes.login);
 
