7a15c1e3350fb0121a2c66388d882c1226483710
Leckller <7leckler@gmail.com>
2024-07-11 15:45:21 -0300

    feat: environment

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,11 +1,11 @@
 import { Options } from "sequelize";
 
 const config: Options = {
-  database: process.env.ENV_DATABASE || 'makeUrTasks',
-  password: process.env.ENV_PASSWORD || 'password',
-  host: process.env.ENV_HOST || 'localhost',
-  username: process.env.ENV_USER || 'root',
-  port: Number(process.env.ENV_PORT) || 3307,
+  database: process.env.MYSQL_DATABASE || 'makeUrTasks',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.MYSQL_HOST || 'localhost',
+  username: process.env.MYSQL_USER || 'root',
+  port: Number(process.env.MYSQL_PORT) || 3307,
   dialect: 'mysql'
 }
 
